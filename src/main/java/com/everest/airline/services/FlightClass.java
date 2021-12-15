aec878ca15050c46ff6b02db434d3ff74d5773dc
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2021-12-15 22:52:55 +0530

    created class of flights

---
+++ src/main/java/com/everest/airline/services/FlightClass.java
@@ -0,0 +1,23 @@
+package com.everest.airline.services;
+
+
+import com.everest.airline.model.Flight;
+
+public abstract class FlightClass {
+    private int seatsAvailable;
+    protected int numberOfPassengers;
+
+    public FlightClass(int seatsType, int numberOfPassengers) {
+        this.seatsAvailable = seatsType;
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public String getDecrementedSeats() {
+        if (seatsAvailable >= numberOfPassengers) {
+            seatsAvailable = seatsAvailable - numberOfPassengers;
+        }
+        return Integer.toString(seatsAvailable);
+    }
+
+    abstract String updatedData(Flight flight, String decreasedSeats);
+}
