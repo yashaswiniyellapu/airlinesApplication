8141e5afbbb2d570c739f55b73b219e53deed264
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2021-12-21 11:28:14 +0530

    updated search service

--- src/main/java/com/everest/airline/services/SearchService.java
+++ src/main/java/com/everest/airline/services/SearchService.java
@@ -1,14 +1,10 @@
 package com.everest.airline.services;
 
-import com.everest.airline.database.DataParser;
 import com.everest.airline.database.FileHandler;
-import com.everest.airline.enums.FareType;
 import com.everest.airline.model.Flight;
-import com.everest.airline.model.FlightFilterTest;
-import com.everest.airline.price.Test;
+import com.everest.airline.price.TotalFareCalculation;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -17,15 +13,14 @@ import java.util.List;
 
 @Component
 public class SearchService {
-    private double price;
+    private double totalFare;
 
-    public List<Flight> flight(String source, String destination, String date,String passengersCount,String classType) throws Exception {
+    public List<Flight> flight(String source, String destination, String date, String passengersCount, String classType) throws Exception {
         List<String[]> fileData;
         List<Flight> flightList = new ArrayList<>();
         FileHandler fileHandler = new FileHandler(source, destination, date);
         fileData = fileHandler.getFileData(Path.of("/Users/yashaswiniyellapu/Documents/airlines/src/main/java/com/everest/airline/database/flightsData"));
-        for(String[] name:fileData)
-        {
+        for (String[] name : fileData) {
             long flightNumber = Long.parseLong(name[0]);
             String from = name[1];
             String to = name[2];
@@ -37,28 +32,26 @@ public class SearchService {
             int secondClassSeats = Integer.parseInt(name[8]);
             int firstClassSeats = Integer.parseInt(name[9]);
             int economicCapacity = Integer.parseInt(name[10]);
-            int numberOfPassengers= Integer.parseInt(passengersCount);
+            int numberOfPassengers = Integer.parseInt(passengersCount);
             int firstClassCapacity = Integer.parseInt(name[11]);
             int secondClassCapacity = Integer.parseInt(name[12]);
 
-            if(new ValidateData().checkFlightData(numberOfPassengers,classType,economicClassSeats,firstClassSeats,secondClassSeats))
-            {
-                flightList.add(new Flight(flightNumber, from, to, departureDate, departureTime, arrivalTime, availableSeats, economicClassSeats, secondClassSeats, firstClassSeats,economicCapacity,secondClassCapacity,firstClassCapacity));
-                Test test = new Test(economicCapacity,secondClassSeats,firstClassSeats);
-                price =test.basePrice(economicClassSeats);
-                FlightFilterTest filter = new FlightFilterTest(classType);
-                System.out.println(filter.filterFlight(Integer.parseInt(passengersCount),economicClassSeats,secondClassSeats,firstClassSeats,"fareCalculation"));
+            if (new ValidateData().checkFlightData(numberOfPassengers, classType, economicClassSeats, firstClassSeats, secondClassSeats)) {
+                flightList.add(new Flight(flightNumber, from, to, departureDate, departureTime, arrivalTime, availableSeats, economicClassSeats, secondClassSeats, firstClassSeats, economicCapacity, secondClassCapacity, firstClassCapacity));
+                TotalFareCalculation fare = new TotalFareCalculation(economicCapacity, secondClassCapacity, firstClassCapacity, economicClassSeats, secondClassSeats, firstClassSeats);
+                totalFare = fare.getTotalFare(classType);
 
+            } else {
+                throw new IllegalStateException();
             }
-            else{throw new IllegalStateException();}
 
         }
 
         return flightList;
     }
-    public double getPrice()
-    {
-          return price;
+
+    public double getTotalFare() {
+        return totalFare;
     }
 }
 
