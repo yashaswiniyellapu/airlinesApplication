801ad002700707f9b33673880ad547861b037b79
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2021-12-22 12:23:45 +0530

    removed redundant code

--- src/main/java/com/everest/airline/services/BookService.java
+++ src/main/java/com/everest/airline/services/BookService.java
@@ -1,41 +1,56 @@
 package com.everest.airline.services;
 
-import com.everest.airline.database.DataParser;
+import com.everest.airline.database.DataRetriever;
+import com.everest.airline.views.FlightClassFilter;
 import com.everest.airline.model.Flight;
-import com.everest.airline.views.FlightClassFliter;
+import com.everest.airline.views.FlightClassType;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 @Component
 public class BookService {
+    FlightClassFilter filter;
+    FlightClassType test;
 
-    public void updateData(Long flightNumber, String classType, String passengerCount, List<Flight> flightData) throws Exception {
-        for (Flight flight : flightData) {
+    public void updateData(Long flightNumber, String classType, String passengerCount, List<Flight> flightData) throws IOException {
+        for(Flight flight:flightData)
+        {
+            String line;
             String updatedLine;
             File directory = new File("/Users/yashaswiniyellapu/Documents/airlines/src/main/java/com/everest/airline/database/flightsData");
             File[] listOfFiles = directory.listFiles();
             if (listOfFiles != null) {
                 for (File file : listOfFiles) {
-                    if (file.getName().equalsIgnoreCase(flightNumber + ".txt")) ;
-                    DataParser data = new DataParser(file);
-                    String line = data.readFile();
-                    if (file.getName().equalsIgnoreCase(flightNumber + ".txt") && (flight.getNumber() == flightNumber)) {
-                        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-                        updatedLine = new FlightClassFliter().seatsLeft(classType, passengerCount, flight);
-                        line = line.replace(line, updatedLine);
-                        writer.write(line);
-                        writer.close();
-                        break;
+                    DataRetriever data = new DataRetriever(file);
+                    if(file.getName().equalsIgnoreCase(flightNumber + ".txt"))
+                    {
+                        line = data.readFile();
+                        filter= new FlightClassFilter(classType);
+                        test= filter.filterClass(flight);
+                            updatedLine = test.getUpdatedData(flightNumber,Integer.parseInt(passengerCount));
+                            if(updatedLine!=null)
+                            {
+                                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+                                line = line.replace(line, updatedLine);
+                                writer.write(line);
+                                writer.close();
+                                break;
+                            }
+                            else {
+                                System.out.println("line is null");
+                            }
+                        }
                     }
-                }
 
+                }
             }
         }
-    }
+
 
 }
 
