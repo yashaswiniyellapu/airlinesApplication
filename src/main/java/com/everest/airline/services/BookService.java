145c9e4d700ba304b60990bc2faf9a99db4df8a2
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-01-26 20:01:02 +0530

    updated booking service

--- src/main/java/com/everest/airline/services/BookService.java
+++ src/main/java/com/everest/airline/services/BookService.java
@@ -1,5 +1,6 @@
 package com.everest.airline.services;
 
+import com.everest.airline.accesslayer.FlightDao;
 import com.everest.airline.database.DirectoryReader;
 import com.everest.airline.model.Flight;
 import org.springframework.stereotype.Component;
@@ -12,16 +13,7 @@ import java.util.List;
 public class BookService {
 
     public void updateData(Long flightNumber, String classType, int passengerCount, List<Flight> flightData) {
-        flightData.parallelStream().forEach(flight -> DirectoryReader.ListOfFiles()
-                .stream()
-                .filter(file -> file.getValue().contains(Long.toString(flightNumber)))
-                .forEach(file -> {
-                    try (BufferedWriter bw = new BufferedWriter(new FileWriter(file.getValue()))) {
-                        bw.write(flight.toString(passengerCount, classType));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }));
+        new FlightDao().update(flightNumber,classType,passengerCount);
     }
 }
 
