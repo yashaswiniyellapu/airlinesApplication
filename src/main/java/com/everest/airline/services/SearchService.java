ee59a2552dd71c2d8eb0799ca157488b8f3ad892
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-01-26 16:37:10 +0530

    added search implementation

--- src/main/java/com/everest/airline/services/SearchService.java
+++ src/main/java/com/everest/airline/services/SearchService.java
@@ -1,49 +1,31 @@
 package com.everest.airline.services;
 
-import com.everest.airline.database.DataReader;
-import com.everest.airline.exceptions.SeatsUnavailableException;
+import com.everest.airline.accesslayer.FlightDao;
 import com.everest.airline.model.Flight;
 import com.everest.airline.price.TotalFareCalculation;
-import com.everest.airline.views.EconomicClass;
-import com.everest.airline.views.FirstClass;
 import com.everest.airline.views.FlightClassType;
-import com.everest.airline.views.SecondClass;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
 public class SearchService {
 
-    public void flightObjectTest
+    public List<Flight> flightObjectTest
             (String source, String destination, String date, int passengersCount, String classType) {
 
+        return new FlightDao().getAll(source, destination, date).stream().map(flight -> {
+            FlightClassType flightClass = flight.flightClassesData(classType);
+            if (flightClass.validateSeats(passengersCount, flightClass.getSeatsLeft())) {
+                TotalFareCalculation classFare = new
+                        TotalFareCalculation(flight.flightClassesData(classType), Date.valueOf(date), classType);
+                flightClass.setClassFare(classFare.byDays());
+                flightClass.setTotalFare(classFare.byDays() * passengersCount);
+            }
+            return flight;
+        }).collect(Collectors.toList());
 
     }
-
-    private boolean validateSeats(int passengersCount, int availableSeats) {
-        return passengersCount <= availableSeats;
-    }
-
 }
-
-////  public Flight(long number, String source, String destination, LocalDate departureDate,LocalTime departureTime,LocalTime arrivalTime, FlightClassType economicClass, FlightClassType secondClass, FlightClassType firstClass) {
-////            long flightNumber = Long.parseLong(name[0]);
-////            String from = name[1];
-////            String to = name[2];
-////            LocalDate departureDate = LocalDate.parse(name[3]);
-////            LocalTime departureTime = LocalTime.parse(name[4]);
-////            LocalTime arrivalTime = LocalTime.parse(name[5]);
-////            int availableSeats = Integer.parseInt(name[6]);
-////            int economicClassSeats = Integer.parseInt(name[7]);
-////            int secondClassSeats = Integer.parseInt(name[8]);
-////            int firstClassSeats = Integer.parseInt(name[9]);
-////            int economicCapacity = Integer.parseInt(name[10]);
-////            int numberOfPassengers = Integer.parseInt(passengersCount);
-////            int firstClassCapacity = Integer.parseInt(name[11]);
-////            int secondClassCapacity = Integer.parseInt(name[12]);
